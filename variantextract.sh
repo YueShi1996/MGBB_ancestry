eb1e63a08c4cf7722ce44c3bcb7d4872164c4af0
YueShi1996 <147172642+YueShi1996@users.noreply.github.com>
2023-11-13 10:54:23 -0500

    Update variantextract.sh

--- variantextract.sh
+++ variantextract.sh
@@ -10,27 +10,26 @@
 
 #SBATCH --error=log%J.err 
 
+folders=("0410" "0411" "0412" "0413")
 
-module load bcftools/1.11
 module load Plink/2.0
 
-cd /PHShome/ys724/Documents/GMBI_endometriosis/pca/
+for fl in "${folders[@]}"; do
 
-for chr in {1..22};do
+tmpdir=/PHShome/ys724/scratch/PCA/${fl}
+mkdir ${tmpdir}
+cd /data/biobank_release/datasets/${fl}/vcf
 
-bcftools view -Oz \
-  -i “ID = @variants.extract” \
-  /data/biobank_release/datasets/0410/vcf/chr${chr}.dose.vcf.gz \
-  > [/PHShome/ys724/scratch/pca/chr${chr}.extracted.vcf.gz]
-
-done
-
-cd /PHShome/ys724/scratch/pca/
-
-bcftools concat -Oz chr{1..22}.extracted.vcf.gz > allchromosomes_0410.vcf.gz
+for chr in $(seq 1 22);do
 
 plink2 \
-  --vcf allchromosomes_0410.vcf.gz \
+  --vcf chr${chr}.dose.vcf.gz \
   dosage=DS \
   --make-pfile \
-  --out allchromosomes_0410
+  --extract /PHShome/ys724/Documents/GMBI_endometriosis/pca/variants.extract \
+  --out ${tmpdir}/chr${chr}_dosage_extract
+
+zcat chr${chr}.info.gz | awk '$7 > 0.3 {print $1}' >  ${tmpdir}/chr${chr}_info_0.3.snplist
+  
+done
+done
