107222ed3e2158b115829f8a16e40e94e659f83f
YueShi1996 <147172642+YueShi1996@users.noreply.github.com>
2023-11-13 11:10:13 -0500

    Create project_pc.sh

---
+++ project_pc.sh
@@ -0,0 +1,13 @@
+cd /PHShome/ys724/scratch/pca/merge/
+mkdir /PHShome/ys724/scratch/pca/projectpc
+
+plink2 \
+  --pfile 0410_merge \
+  --score /PHShome/ys724/Documents/GMBI_endometriosis/pca/hgdp_tgp_pca_gbmi_snps_loadings.GRCh38.plink.tsv \
+  variance-standardize \
+  cols=-scoreavgs,+scoresums \
+  list-variants \
+  header-read \
+  --score-col-nums 3-22 \
+  --read-freq /PHShome/ys724/Documents/GMBI_endometriosis/pca/hgdp_tgp_pca_gbmi_snps_loadings.GRCh38.plink.afreq \
+  --out /PHShome/ys724/scratch/pca/projectpc/0410_projectpc
