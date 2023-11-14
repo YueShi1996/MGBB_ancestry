1f1fbb322919bdc534e66132bdb417e6cfd85ef9
YueShi1996 <147172642+YueShi1996@users.noreply.github.com>
2023-11-14 15:38:50 -0500

    Update merge_folder.sh

--- merge_folder.sh
+++ merge_folder.sh
@@ -22,3 +22,8 @@ done
 comm -12 0410_snplist_sorted.txt 0411_snplist_sorted.txt > common_0410_0411.txt
 comm -12 common_0410_0411.txt 0412_snplist_sorted.txt > common_0410_0411_0412.txt
 comm -12 common_0410_0411_0412.txt 0413_snplist_sorted.txt > common_all.txt
+
+for fl in "${folders[@]}"; do
+module load Plink/2.0
+plink2 --pfile 
+done
