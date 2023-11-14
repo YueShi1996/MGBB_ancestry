2472d6df9004186659be20ea4e511bf568fd2b46
YueShi1996 <147172642+YueShi1996@users.noreply.github.com>
2023-11-14 15:30:29 -0500

    Create merge_folder.sh

---
+++ merge_folder.sh
@@ -0,0 +1,14 @@
+
+
+cd /PHShome/ys724/scratch/pca/merge/
+
+folders=("0410" "0411" "0412" "0413")
+
+for fl in "${folders[@]}"; do
+awk '{print $3}' ${fl}_merge.pvar > ${fl}_snplist.txt
+sort ${fl}_snplist.txt > ${fl}_snplist_sorted.txt
+done
+
+comm -12 0410_snplist_sorted.txt 0411_snplist_sorted.txt > common_0410_0411.txt
+comm -12 common_0410_0411.txt 0412_snplist_sorted.txt > common_0410_0411_0412.txt
+comm -12 common_0410_0411_0412.txt 0413_snplist_sorted.txt > common_all.txt
